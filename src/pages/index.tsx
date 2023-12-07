4cad8d7a3e33bd67022f76f17e1e2232fe95b60f
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-12-07 17:32:58 -0600

    add viewer config file 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
 import dynamic from "next/dynamic";
-import {
-  viewerComponent,
-  viewerOptions,
-  type ViewerOptions,
-} from "@/viewerConfig";
-import { type CustomDisplay } from "@samvera/clover-iiif";
+import { formatCustomDisplays, type APIData } from "@/viewerConfig";
 
 const Viewer = dynamic(
   () =>
@@ -16,50 +11,24 @@ const Viewer = dynamic(
   }
 );
 
-let pdfManifests = [
-  { manifestUrl: "http://localhost:3000/manifests/pdf.json", target: "item1" },
-  {
-    manifestUrl:
-      "https://seagate.whirl-i-gig.com/admin/service/IIIF/manifest/ca_objects:170",
-    target:
-      "https://seagate.whirl-i-gig.com/admin/service/IIIF/manifest/ca_objects:170-255",
-  },
-];
-let pdfManifest = pdfManifests[1];
-
-let apiData = {
-  manifestUrl: pdfManifest.manifestUrl,
+let apiData: APIData = {
+  manifestUrl:
+    "https://seagate.whirl-i-gig.com/admin/service/IIIF/manifest/ca_objects:170",
   customViewers: [
     {
-      itemId: pdfManifest.target,
-      viewer: "pdf",
+      itemId:
+        "https://seagate.whirl-i-gig.com/admin/service/IIIF/manifest/ca_objects:170-255",
+      viewerType: "pdf",
     },
   ],
 };
-
-let customDisplays = apiData.customViewers
-  .filter((viewer) => {
-    return viewerComponent[viewer.viewer as keyof ViewerOptions] !== undefined;
-  })
-  .map((viewer) => {
-    let data = {
-      target: viewer.itemId,
-      component: viewerComponent[viewer.viewer as keyof ViewerOptions],
-    } as CustomDisplay;
-    if (viewerOptions[viewer.viewer as keyof ViewerOptions]) {
-      data.options = viewerOptions[viewer.viewer as keyof ViewerOptions];
-    }
-    return data;
-  });
-
 
 export default function Home() {
   return (
     <>
       <Viewer
         iiifContent={apiData.manifestUrl}
-        options={{ informationPanel: { renderAbout: true } }}
-        customDisplays={customDisplays}
+        customDisplays={formatCustomDisplays(apiData)}
       />
       ;
     </>
--- src/viewerConfig.tsx
+++ src/viewerConfig.tsx
@@ -1,3 +1,5 @@
+import { type CustomDisplay } from "@samvera/clover-iiif";
+
 import PdfViewer from "@/components/PdfViewer/PdfViewer";
 
 export type ViewerOptions = {
@@ -24,3 +26,30 @@ export type ViewerComponent = {
 export const viewerComponent: ViewerComponent = {
   pdf: PdfViewer,
 };
+
+export type APIData = {
+  manifestUrl: string;
+  customViewers: { itemId: string; viewerType: string }[];
+};
+
+export function formatCustomDisplays(apiData: APIData) {
+  return apiData.customViewers
+    .filter((viewer) => {
+      return (
+        viewerComponent[viewer.viewerType as keyof ViewerOptions] !== undefined
+      );
+    })
+    .map((viewer) => {
+      let viewerType = viewer.viewerType as keyof ViewerOptions;
+      let data = {
+        target: viewer.itemId,
+        component: viewerComponent[viewerType],
+      } as CustomDisplay;
+
+      if (viewerOptions[viewerType]) {
+        data.options = viewerOptions[viewerType];
+      }
+
+      return data;
+    });
+}
